6af35c24baf4cb3565651eedf4b794c7c3b80c72
songyan-hou <815759459@qq.com>
2026-06-03 22:13:16 +0200

    new lesson

---
+++ data/lesson8.js
@@ -0,0 +1,18 @@
+window.lessonData = {
+  title: "2026-06-04 短元音 /e/ 挨",
+  words: [
+    { en: "leg",   zh: "大腿",  pro: "赖个",    com: "" },
+    { en: "bed",   zh: "床",    pro: "败的",    com: "" },
+    { en: "fresh", zh: "新鲜的", pro: "抚rai施", com: "" },
+    { en: "desk",  zh: "桌子",  pro: "带死可",   com: "" },
+    { en: "table",  zh: "桌子",  pro: "tei bou",   com: "" },
+    { en: "tell",  zh: "告诉",  pro: "胎藕",    com: "" },
+    { en: "smell", zh: "闻",    pro: "思麦偶",   com: "" },
+    { en: "bell",  zh: "铃",    pro: "贝偶",    com: "" },
+    { en: "red",   zh: "红色",  pro: "入爱的",   com: "" },
+    { en: "wet",   zh: "湿的",  pro: "外特",    com: "" },
+    { en: "ten",   zh: "十",    pro: "特恩",    com: "" },
+    { en: "end",   zh: "结束",  pro: "恩的",    com: "" },
+    { en: "rest",  zh: "休息",  pro: "入爱思特", com: "" },
+  ]
+};
---
+++ data/lesson9.js
@@ -0,0 +1,20 @@
+window.lessonData = {
+  title: "2026-06-04 短元音 /ɪ/ 意",
+  words: [
+    { en: "milk",   zh: "牛奶",      pro: "密偶可",  com: "" },
+    { en: "soymilk",   zh: "豆浆",      pro: "",  com: "" },
+    { en: "soy",   zh: "大豆",      pro: "",  com: "" },
+    { en: "fish",   zh: "鱼",        pro: "抚易施",  com: "" },
+    { en: "swim",   zh: "游泳",      pro: "思五一母", com: "" },
+    { en: "drink",  zh: "喝",        pro: "坠恩可",  com: "" },
+    { en: "sit",    zh: "坐",        pro: "思义特",  com: "" },
+    { en: "happy",  zh: "开心的",    pro: "嗨匹",    com: "" },
+    { en: "marry",  zh: "结婚",      pro: "麦瑞",  com: "" },
+    { en: "rainy",  zh: "下雨的",    pro: "瑞你",    com: "" },
+    { en: "smelly", zh: "有气味的",  pro: "思麦里",  com: "" },
+    { en: "exam",   zh: "考试",      pro: "一个赞母", com: "" },
+    { en: "repair", zh: "修理",      pro: "瑞派儿",  com: "" },
+    { en: "behind", zh: "在……后面",  pro: "比汗的",  com: "" },
+    { en: "I drink soymilk. I am happy.", zh: "我喝豆浆。我开心。",  pro: "",  com: "" },
+  ]
+};

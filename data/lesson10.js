4698ac8fc4e3fdb2d0c63505cf27981db479a42d
songyan-hou <815759459@qq.com>
2026-06-04 18:27:42 +0200

    new lesson

---
+++ data/lesson10.js
@@ -0,0 +1,22 @@
+window.lessonData = {
+  title: "2026-06-05 短元音 /ɒ/ 哦",
+  words: [
+    { en: "ox",    zh: "公牛",  pro: "哦可思",   com: "" },
+    { en: "oxford",    zh: "牛津",  pro: "",   com: "" },
+    { en: "clock", zh: "钟表",  pro: "克落克",   com: "" },
+    { en: "sock",  zh: "短袜",  pro: "思傲可",   com: "" },
+    { en: "stock",  zh: "股票",  pro: "思道可",   com: "" },
+    { en: "shop",  zh: "商店",  pro: "少普",     com: "" },
+    { en: "dog",   zh: "狗",    pro: "盗哥",     com: "" },
+    { en: "hot",   zh: "热的",  pro: "浩特",     com: "" },
+    { en: "hotdog",   zh: "热狗",  pro: "",     com: "" },
+    { en: "fox",   zh: "狐狸",  pro: "抚傲可思", com: "" },
+    { en: "top",   zh: "顶上",  pro: "套普",     com: "" },
+    { en: "boss",  zh: "老板",  pro: "报斯",     com: "" },
+    { en: "pot",   zh: "汤锅",    pro: "泡特",     com: "" },
+    { en: "pan",   zh: "平底锅",    pro: "潘",     com: "" },
+    { en: "wok",   zh: "炒锅",    pro: "vok",     com: "" },
+    { en: "box",   zh: "盒子",  pro: "抱可思",   com: "" },
+    { en: "dot",   zh: "点",    pro: "盗特",     com: "" },
+  ]
+};
---
+++ data/lesson11.js
@@ -0,0 +1,19 @@
+window.lessonData = {
+  title: "2026-06-05 短音 /ʊ/ 鸣",
+  words: [
+    { en: "book",   zh: "书",   pro: "不可",   com: "" },
+    { en: "read",   zh: "读",   pro: "",   com: "" },
+    { en: "good",   zh: "好的", pro: "固的",   com: "" },
+    { en: "foot",   zh: "脚",   pro: "付特",   com: "" },
+    { en: "wood",   zh: "木材", pro: "雾的",   com: "" },
+    { en: "cook",   zh: "厨师", pro: "酷可",   com: "" },
+    { en: "woman",  zh: "妇女", pro: "屋门",   com: "" },
+    { en: "wolf",   zh: "狼",   pro: "勿偶服", com: "" },
+    { en: "who",    zh: "谁",   pro: "护",     com: "" },
+    { en: "put",    zh: "放",   pro: "铺特",   com: "" },
+    { en: "pull",   zh: "拉",   pro: "铺偶",   com: "" },
+    { en: "push",   zh: "推",   pro: "铺施",   com: "" },
+    { en: "full",   zh: "满的", pro: "付偶",   com: "" },
+    { en: "would",  zh: "将会", pro: "雾的",   com: "" },
+  ]
+};
